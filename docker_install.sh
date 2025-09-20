4cdb1e53c26fa052cf4ac378498f7a6840b1dc10
Vedansh kumar <kumarvedansh134@gmail.com>
2025-09-20 22:51:22 +0530

    add docker function

--- docker_install.sh
+++ docker_install.sh
@@ -1,60 +1,76 @@
 #!/bin/bash
-
+#
 # ============================ docker installation ============================
+#
+# testing (change to set -xeou pipefail for testing)
+set +xeou pipefail
+
+# define variables
+SUDO='sudo'
 
 echo " 🚀 Installation of Docker started"
 echo ""
 
 # Update ubuntu
 echo " 🛠️ Updating Ubuntu packages..."
-sudo apt update
+$SUDO apt update
 echo ""
 
 # Install required packages
 # echo " 📦 Installing required packages..."
-# sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+# $SUDO apt install apt-transport-https ca-certificates curl software-properties-common -y
 # echo ""
 
 # # Add Docker’s official GPG key
 # echo " 🔑 Adding Docker’s official GPG key..."
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | $SUDO apt-key add -
 # echo ""
 
 # # Add Docker repository
 # echo " 📦 Adding Docker repository..."
-# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# $SUDO add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 # echo ""
 
 # # Update the package list
 # echo " 🛠️ Updating package list..."
-# sudo apt update
+# $SUDO apt update
 # echo ""
 
-# Install Docker
-echo " ✅ Installing Docker..."
-sudo apt install docker.io -y
-echo ""
+# function to install docker
+install_docker(){
+    echo "✅ Installing Docker..."
+    if command -v docker >/dev/null 2>&1; then
+        echo "Docker is alreaady installed"
+    else
+        echo "🚫 Docker is not installed, starting installation..."
+        $SUDO apt install docker.io -y
+        echo ""
+    fi
+}
+
+# call docker function
+install_docker
 
 # Start Docker service
 echo " 🔄 Starting Docker service..."
-sudo systemctl start docker
+$SUDO systemctl start docker
 echo ""
 
 # Enable Docker to start at boot
 echo " 🔧 Enabling Docker to start at boot..."
-sudo systemctl enable docker
+$SUDO systemctl enable docker
 echo ""
 
 # Allow non-root users to run Docker commands
-sudo chmod 777 /var/run/docker.sock
+$SUDO chmod 666 /var/run/docker.sock
 
 # Add user to the Docker group
-sudo usermod -aG docker $USER && newgrp docker
+$SUDO usermod -aG docker $USER && newgrp docker
 
 
 echo " 🎉 Docker installation completed."
 echo "Docker version:"
-sudo docker --version
+$SUDO docker --version
 
 
 
