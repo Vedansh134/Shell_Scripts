27e1fe87f619d4c17832070c651cf3a0140cf2e8
Vedansh kumar <kumarvedansh134@gmail.com>
2025-09-24 23:38:37 +0530

    Add function to install eksctl

--- monitoring/eks_install.sh
+++ monitoring/eks_install.sh
@@ -29,3 +29,84 @@ if aws sts get-caller-identity >/dev/null 2>&1; then
 else
     echo " ❌ AWS is not configured. Firstly configure AWS"
 fi
+
+# check kubectl is installed or not
+if command -v kubectl >/dev/null 2>&1; then
+    echo " ✅ kubectl is already installed."
+else
+    echo " ❌ kubectl is not installed. Proceeding with installation..."
+    # Download the latest release of kubectl
+    curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
+
+    # Make the kubectl binary executable
+    chmod +x ./kubectl
+
+    # Move the binary to a directory in your PATH
+    $SUDO mv ./kubectl /usr/local/bin
+
+    # Verify the installation
+    kubectl version --short --client
+fi
+
+# check eksctl is installed or not
+if command -v eksctl >/dev/null 2>&1; then
+    echo " ✅ eksctl is already installed."
+else
+    echo " ❌ eksctl is not installed. Proceeding with installation..."
+    # Download the latest release of eksctl
+    curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+    # Move the binary to a directory in your PATH
+    $SUDO mv /tmp/eksctl /usr/local/bin
+
+    # Verify the installation
+    eksctl version
+fi
+
+# EKS cluster function
+eks_cluster_create() {
+    echo " 🚀 EKS cluster creation started"
+    echo ""
+    read -p "Enter the cluster name: " CLUSTER_NAME
+    read -p "Enter the region (e.g. ap-south-1): " REGION
+    read -p "Enter the number of zones (e.g. ap-south-1a,ap-south-1b give atleast 2): " ZONES
+
+    echo " 🔧 Creating EKS cluster..."
+    eksctl create cluster --name=$CLUSTER_NAME \
+                          --region=$REGION \
+                          --zones=$ZONES \
+                          --without-nodegroup
+
+    echo " ✅ EKS cluster '$CLUSTER_NAME' created successfully in region '$REGION'."
+}
+
+# Call the EKS cluster creation function
+eks_cluster_create
+
+# Associate IAM OIDC provider with the EKS cluster
+echo " 🔧 Associating IAM OIDC provider with the EKS cluster..."
+eksctl utils associate-iam-oidc-provider \
+    --region $REGION \
+    --cluster $CLUSTER_NAME \
+    --approve
+
+# Node group function
+create_node_group() {
+    eksctl create nodegroup --cluster=observability \
+                        --region=us-east-1 \
+                        --name=observability-ng-private \
+                        --node-type=t3.medium \
+                        --nodes-min=2 \
+                        --nodes-max=3 \
+                        --node-volume-size=20 \
+                        --managed \
+                        --asg-access \
+                        --external-dns-access \
+                        --full-ecr-access \
+                        --appmesh-access \
+                        --alb-ingress-access \
+                        --node-private-networking
+}
+
+# Update ./kube/config file
+aws eks update-kubeconfig --name observability
